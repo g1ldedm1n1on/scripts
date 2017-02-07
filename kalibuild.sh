ef0cacdfffe166b014966d0f3e638005018d584e
g1ldedm1n1on <g1ldedm1n1on@gmail.com>
2017-02-07 15:31:22 -0600

    added kali build script

--- kalibuild.sh
+++ kalibuild.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
+printf "\nInstalling VMwareTools
 apt-get install open-vm-tools-desktop fuse
+
+
 
 
 mkdir /root/git_repos
 cd /root/git_repos
 
-echo /n*********Cloning Git Repos********
+printf /n*********Cloning Git Repos********
 git clone https://github.com/EmpireProject/Empire.git
 git clone https://github.com/EmpireProject/EmPyre.git
 git clone https://github.com/Sw4mpf0x/Kraken.git
 git clone https://github.com/g1ldedm1n1on/scripts.git
 git clone https://github.com/g1ldedm1n1on/Kaliupdater.git
 git clone https://github.com/g1ldedm1n1on/msf_resourcefiles.git
-echo “/n*******Installing Vulscan********”
 
+printf “\n*******Installing Vulscan********”
 
 cd /root/Downloads
 wget http://www.computec.ch/projekte/vulscan/download/nmap_nse_vulscan-2.0.tar.gz
@@ -22,14 +25,14 @@ wget http://www.computec.ch/projekte/vulscan/download/nmap_nse_vulscan-2.0.tar.g
 tar xfz nmap_nse_vulscan-2.0.tar.gz
 mv vulscan/ /usr/share/nmap/scripts/
 
-
+printf "\n*********Installing Kraken*******"
 cd /root/git_repos/Kraken
 chmod 755 ./setup.sh
 ./setup.sh
 
 mkdir /root/git_repos/powershell
 cd /root/git_repos/powershell
-echo “/n******Installing Powershell Repos*******”
+printf “\n******Installing Powershell Repos*******”
 git clone https://github.com/NetSPI/PowerUpSQL.git
 git clone https://github.com/samratashok/nishang.git
 git clone https://github.com/Kevin-Robertson/Inveigh.git
@@ -44,4 +47,4 @@ chmod +x kaliupdater.sh
 
 
 
-apt-get install shelter
+apt-get install shellter
