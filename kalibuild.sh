7cd5522d83cc2395f00c689ff9b76c1bfb727efa
g1ldedm1n1on <g1ldedm1n1on@gmail.com>
2017-02-08 10:21:38 -0600

    updated kali build script

--- kalibuild.sh
+++ kalibuild.sh
@@ -3,7 +3,7 @@
 
 
 
-printf "\n\e[32m***********Installing VMwareTools************\n"
+printf "\n\e[32m***********Installing VMwareTools************\n\e[0m"
 apt-get install open-vm-tools-desktop fuse -Y
 
 
@@ -12,7 +12,7 @@ apt-get install open-vm-tools-desktop fuse -Y
 mkdir /root/git_repos
 cd /root/git_repos
 
-printf "\n\e[32m*********Cloning Git Repos********\n"
+printf "\n\e[32m*********Cloning Git Repos********\n\e[0m"
 git clone https://github.com/EmpireProject/Empire.git
 git clone https://github.com/EmpireProject/EmPyre.git
 git clone https://github.com/Sw4mpf0x/Kraken.git
@@ -20,7 +20,7 @@ git clone https://github.com/g1ldedm1n1on/scripts.git
 git clone https://github.com/g1ldedm1n1on/Kaliupdater.git
 git clone https://github.com/g1ldedm1n1on/msf_resourcefiles.git
 
-printf “\n\e[32m*******Installing Vulscan********\n”
+printf “\n\e[32m*******Installing Vulscan********\n\e[0m”
 
 cd /root/Downloads
 wget http://www.computec.ch/projekte/vulscan/download/nmap_nse_vulscan-2.0.tar.gz
@@ -28,7 +28,7 @@ wget http://www.computec.ch/projekte/vulscan/download/nmap_nse_vulscan-2.0.tar.g
 tar xfz nmap_nse_vulscan-2.0.tar.gz
 mv vulscan/ /usr/share/nmap/scripts/
 
-printf "\n\e[32m*********Installing Kraken*********\n"
+printf "\n\e[32m*********Installing Kraken*********\n\e[0m"
 cd /root/git_repos/Kraken
 chmod 755 ./setup.sh
 ./setup.sh
@@ -36,7 +36,7 @@ chmod 755 ./setup.sh
 mkdir /root/git_repos/powershell
 cd /root/git_repos/powershell
 
-printf “\n\e[32m******Installing Powershell Repos*******\n”
+printf “\n\e[32m******Installing Powershell Repos*******\n\e[0m”
 git clone https://github.com/NetSPI/PowerUpSQL.git
 git clone https://github.com/samratashok/nishang.git
 git clone https://github.com/Kevin-Robertson/Inveigh.git
@@ -49,12 +49,12 @@ chmod +x kaliupdater.sh
 ./kaliupdater.sh
 
 
-printf "\n\e[32m******Installing Shellter*******\n"
+printf "\n\e[32m******Installing Shellter*******\n\e[0m"
 
 apt-get install shellter veil-evasion
 
 printf "\n\e[32m*****Installing Veil Evasion**********\n"
-printf "\n\e[32m]Please Click through the Defaults on WINE Installs!"
+printf "\nPlease Click through the Defaults on WINE Installs!\e[0m"
 
 cd /usr/share/veil-evasion/setup
 ./setup.sh
